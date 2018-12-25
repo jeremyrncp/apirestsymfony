8c4b3b053381e9470ff80b25e955bda9f969f5f9
Jeremy Gaultier <contact@webmezenc.com>
2018-12-25 11:54:50 +0100

    fix(eventlistener): intercept not found exception thrown by param converter

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -6,9 +6,11 @@
 
 namespace App\EventListener;
 
+use App\Enum\HttpCodeEnum;
 use App\Exception\ApiExceptionInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ExceptionListener
 {
@@ -24,6 +26,16 @@ class ExceptionListener
 
             $response->setStatusCode($exception->getCode());
 
+            $event->setResponse($response);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            $response = new Response();
+            $response->setContent(json_encode([
+                'error' => 'Entity not found'
+            ]));
+
+            $response->setStatusCode(HttpCodeEnum::HTTP_NOT_FOUND);
             $event->setResponse($response);
         }
     }
