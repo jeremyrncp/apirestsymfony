77e451b8faa6f1d5ea46a39ee232b73185a08520
Jeremy Gaultier <contact@webmezenc.com>
2019-01-03 20:53:32 +0100

    fix(entity): add relation in phone

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -5,10 +5,19 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
+use Hateoas\Configuration\Annotation as Hateoas;
 
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PhoneRepository")
+ *
+ * @Hateoas\Relation(
+ *      "self",
+ *      href = @Hateoas\Route(
+ *          "view_phone",
+ *          parameters = { "id" = "expr(object.getId())" }
+ *      ),
+ * )
  */
 class Phone
 {
